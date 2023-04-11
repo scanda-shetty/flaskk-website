62a0e0d505cdc2af16bcdb780e0b276f2e53e552
scanda-shetty <skandasshetty@gmail.com>
2023-04-11 13:20:29 +0530

    last commit

--- app.py
+++ app.py
@@ -1,105 +1,55 @@
 import io
 import json
 import os
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-common = {'first_name': 'Peter', 'last_name': 'Parker', 'alias': 'spiderman'}
-
-
 @app.route('/')
 def index():
-  return render_template('home.html', common=common)
+  data = get_static_json("static/files/marvel.json")['projects']
+  return render_template('home.html', projects=data)
 
+@app.route('/', methods=['POST'])
+def search():
+    query = request.form.get('search')
+    data = get_static_json("static/files/marvel.json")['projects']
+    results = []
+    for project in data:
+        if query.lower() in project['name'].lower():
+            results.append(project)
+    return render_template('home.html', results=results, query=query)
 
-@app.route('/timeline')
+@app.route('/<string:name>')
+def project(name):
+  data = get_static_json("static/files/marvel.json")['projects']
+  for project in data:
+        if project['name'] == name:
+            return render_template('project.html', project=project)
+  return 'Entry not found'
+
+@app.route('/movies')
 def timeline():
-  timeline = get_static_json("static/files/timeline.json")
-  return render_template('timeline.html', common=common, timeline=timeline)
+  data = get_static_json("static/files/marvel.json")['projects']
+  return render_template('movie.html', projects=data)
 
+@app.route('/characters')
+def character():
+  data = get_static_json("static/files/char.json")['characters']
+  return render_template('character.html', characters=data)
 
 @app.route('/reading')
-def reading():
-  data = get_static_json("static/files/reading.json")
-  return render_template('reading.html', common=common, data=data)
-
-
-@app.route('/projects')
 def projects():
-  data = get_static_json("static/projects/projects.json")['projects']
-  data.sort(key=order_projects_by_weight, reverse=True)
-
-  tag = request.args.get('tags')
-  if tag is not None:
-    data = [
-      project for project in data if tag.lower() in
-      [project_tag.lower() for project_tag in project['tags']]
-    ]
-
-  return render_template('projects.html',
-                         common=common,
-                         projects=data,
-                         tag=tag)
-
-
-@app.route('/experiences')
-def experiences():
-  experiences = get_static_json(
-    "static/experiences/experiences.json")['experiences']
-  experiences.sort(key=order_projects_by_weight, reverse=True)
-  return render_template('projects.html',
-                         common=common,
-                         projects=experiences,
-                         tag=None)
-
-
-def order_projects_by_weight(projects):
-  try:
-    return int(projects['weight'])
-  except KeyError:
-    return 0
-
-
-@app.route('/projects/<title>')
-def project(title):
-  projects = get_static_json("static/projects/projects.json")['projects']
-  experiences = get_static_json(
-    "static/experiences/experiences.json")['experiences']
-
-  in_project = next((p for p in projects if p['link'] == title), None)
-  in_exp = next((p for p in experiences if p['link'] == title), None)
-
-  if in_project is None and in_exp is None:
-    return render_template('404.html'), 404
-  # fixme: choose the experience one for now, cuz I've done some shite hardcoding here.
-  elif in_project is not None and in_exp is not None:
-    selected = in_exp
-  elif in_project is not None:
-    selected = in_project
-  else:
-    selected = in_exp
-
-  # load html if the json file doesn't contain a description
-  if 'description' not in selected:
-    path = "experiences" if in_exp is not None else "projects"
-    selected['description'] = io.open(get_static_file(
-      'static/%s/%s/%s.html' % (path, selected['link'], selected['link'])),
-                                      "r",
-                                      encoding="utf-8").read()
-  return render_template('project.html', common=common, project=selected)
-
+  data = get_static_json("static/files/marvel.json")['projects']
+  return render_template('reading.html', projects=data)
 
 @app.errorhandler(404)
 def page_not_found(e):
-  return render_template('404.html', common=common), 404
-
+  return render_template('404.html'), 404
 
 def get_static_file(path):
   site_root = os.path.realpath(os.path.dirname(__file__))
   return os.path.join(site_root, path)
-
 
 def get_static_json(path):
   return json.load(open(get_static_file(path)))
--- static/js/home.js
+++ static/js/home.js
@@ -1,27 +1,32 @@
-$(function() {
-  /* NOTE: hard-refresh the browser once you've updated this */
-  $(".typed").typed({
-    strings: [
-      "stat peterparker<br/>" +
-      "><span class='caret'>$</span> job: Friendly Neighborhood Spider-Man<br/> ^100" +
-      "><span class='caret'>$</span> skills: combat spider, villain slayer<br/> ^100" +
-      "><span class='caret'>$</span> hobbies: star-gazer, universe whisperer, mountain-biker<br/> ^300" +
-      "><span class='caret'>$</span> alias: spiderman<br/> ^300" +
-      "><span class='caret'>$</span> universe: found traces in every universe<br/> ^300"
-    ],
-    showCursor: true,
-    cursorChar: '_',
-    autoInsertCss: true,
-    typeSpeed: 0.001,
-    startDelay: 50,
-    loop: false,
-    showCursor: false,
-    onStart: $('.message form').hide(),
-    onStop: $('.message form').show(),
-    onTypingResumed: $('.message form').hide(),
-    onTypingPaused: $('.message form').show(),
-    onComplete: $('.message form').show(),
-    onStringTyped: function(pos, self) {$('.message form').show();},
-  });
-  $('.message form').hide()
+const searchForm = document.querySelector('form');
+const searchBar = document.getElementById('search-bar');
+const projectContainer = document.getElementById('project-container');
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const searchValue = searchBar.value.toLowerCase();
+
+  fetch('marvel.json')
+    .then(response => response.json())
+    .then(data => {
+      projectContainer.innerHTML = '';
+      data.projects.forEach(project => {
+        const projectName = project.name.toLowerCase();
+        if (projectName.includes(searchValue)) {
+          const projectDiv = document.createElement('div');
+          projectDiv.classList.add('project');
+
+          const projectImage = document.createElement('img');
+          projectImage.src = project.photos[0];
+          projectImage.alt = project.name;
+
+          const projectNameElem = document.createElement('h2');
+          projectNameElem.textContent = project.name;
+
+          projectDiv.appendChild(projectImage);
+          projectDiv.appendChild(projectNameElem);
+          projectContainer.appendChild(projectDiv);
+        }
+      });
+    });
 });
